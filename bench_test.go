01dab4536a78a2eb4f98298b9a219c1fccfb978d
kklash <konnor@exodus.io>
2022-01-17 07:24:27 -0800

    do not benchmark big-int parsing

--- bench_test.go
+++ bench_test.go
@@ -9,9 +9,10 @@ func BenchmarkNonceECDSA256(b *testing.B) {
 	fieldFixture := fixtures[4] // ECDSA 256-bit
 	q := NewQ(bigi(fieldFixture.Q))
 	hashed := sha256.Sum256([]byte("whatever"))
+	privKey := bigi(fieldFixture.Privkey)
 
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		q.Nonce(bigi(fieldFixture.Privkey), hashed[:], sha256.New)
+		q.Nonce(privKey, hashed[:], sha256.New)
 	}
 }
